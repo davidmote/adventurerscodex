be2e642d4a422716666a18f91716bc388bda443e
David Mote <davidmote@gmail.com>
2018-03-18 22:22:20 -0700

    spell stats refactor

--- src/charactersheet/viewmodels/character/spell_stats/index.js
+++ src/charactersheet/viewmodels/character/spell_stats/index.js
@@ -5,6 +5,7 @@ import {
 } from 'charactersheet/utilities';
 import { PersistenceService } from 'charactersheet/services/common/persistence_service';
 import { SpellStats } from 'charactersheet/models/character';
+import { SpellStatsFormViewModel} from './form';
 import ko from 'knockout';
 import template from './index.html';
 
@@ -12,61 +13,6 @@ export function SpellStatsViewModel() {
     var self = this;
 
     self.spellStats = ko.observable(new SpellStats());
-    self.modalStatus = ko.observable(false);
-    self.editItem = ko.observable(new SpellStats());
-    self.firstModalElementHasFocus = ko.observable(false);
-    self.editMode = ko.observable(false);
-    self.elementHeight = ko.observable('400px');
-
-    const PANEL_ID = '#spell-stats-panel';
-
-    self.setNewHeight = function () {
-        let setHeight = 0;
-        if (self.editMode()) {
-            setHeight = $(`${PANEL_ID} .back`).height();
-        } else {
-            setHeight = $(`${PANEL_ID} .front`).height();
-        }
-        if (setHeight > 0) {
-            self.elementHeight(setHeight.toString()+'px');
-        }
-    };
-    // Wait for page load
-    setTimeout(self.setNewHeight,0);
-
-    // self.editSpellStats = function() {
-    //     self.modalStatus(true);
-    //     self.editItem(new SpellStats());
-    //     self.editItem().importValues(self.spellStats().exportValues());
-    // };
-    //
-    // self.modalFinishedAnimating = function() {
-    //     self.firstModalElementHasFocus(true);
-    //     self.firstModalElementHasFocus.valueHasMutated();
-    // };
-    //
-    // self.modalFinishedClosing = function() {
-    //     if (self.modalStatus()) {
-    //         self.spellStats().importValues(self.editItem().exportValues());
-    //     }
-    //     self.dataHasChanged();
-    //     self.modalStatus(false);
-    // };
-
-
-    self.editStats = function() {
-        if (self.editMode()) {
-            self.spellStats().importValues(self.editItem().exportValues());
-            self.dataHasChanged();
-            self.editMode(false);
-        } else {
-            self.editItem(new SpellStats());
-            self.editItem().importValues(self.spellStats().exportValues());
-            self.editMode(true);
-            self.firstModalElementHasFocus(true);
-            self.firstModalElementHasFocus.valueHasMutated();
-        }
-    };
 
     self.load = function() {
         Notifications.global.save.add(self.save);
@@ -80,7 +26,6 @@ export function SpellStatsViewModel() {
         }
         self.spellStats().characterId(key);
         self.spellStats().spellAttackBonus.subscribe(self.dataHasChanged);
-        self.setNewHeight();
     };
 
     self.unload = function() {
@@ -94,33 +39,6 @@ export function SpellStatsViewModel() {
 
     self.clear = function() {
         self.spellStats().clear();
-    };
-
-    self.setSpellCastingAbility = function(label, value) {
-        self.editItem().spellcastingAbility(label);
-    };
-
-    self.editMode.subscribe(self.setNewHeight);
-
-    // Modal Methods
-
-    self.editSpellStats = function() {
-        self.modalStatus(true);
-        self.editItem(new SpellStats());
-        self.editItem().importValues(self.spellStats().exportValues());
-    };
-
-    self.modalFinishedAnimating = function() {
-        self.firstModalElementHasFocus(true);
-        self.firstModalElementHasFocus.valueHasMutated();
-    };
-
-    self.modalFinishedClosing = function() {
-        if (self.modalStatus()) {
-            self.spellStats().importValues(self.editItem().exportValues());
-        }
-        self.dataHasChanged();
-        self.modalStatus(false);
     };
 
     self.dataHasChanged = function() {
